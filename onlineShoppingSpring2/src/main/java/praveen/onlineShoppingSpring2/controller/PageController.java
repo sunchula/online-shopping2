d5c3bd4d20f2f78d4d050e682c249963ea8c0fd2
sunchula <sunchulapraveen@gmail.com>
2019-02-01 05:50:15 +0530

    Static Resources like Bootstrap, JS, CSS

---
+++ onlineShoppingSpring2/src/main/java/praveen/onlineShoppingSpring2/controller/PageController.java
@@ -0,0 +1,16 @@
+package praveen.onlineShoppingSpring2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Spring MVC!!");
+		return mv;
+	}
+}
